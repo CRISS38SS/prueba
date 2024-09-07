115c47b10e88039189f7ce7190cfa96d5f48d0a9
Cristian Lopez <cl018191@gmail.com>
2024-09-07 17:53:58 -0600

    se terminaron de agregar las operaciones basicas

--- src/itz/prueba.java
+++ src/itz/prueba.java
@@ -1,27 +1,32 @@
 package itz;
 
 import javax.swing.JOptionPane;
+import java.awt.*;
+import javax.swing.*;
 public class prueba {
     public static void main(String[] args) {
         prueba n=new prueba();
     }
     public prueba(){
-        int x=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa un numero"));
-        int y=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa un numero"));
-        int sum=suma(x, y);
-        int res=resta(x, y);
-        int mult=multiplicacion(x,y);
-        JOptionPane.showMessageDialog(null,"la suma es "+sum+", la resta es "+res+" y la multiplicacion es "+mult);
+        
     }
     public int suma(int x,int y){
         return x+y;
     }
-
+    
     public int resta(int x, int y){
         return x-y;
     }
 
     public int multiplicacion(int x, int y){
         return x*y;
+    }
+
+    public  float division(int x,int y){
+        return x/y;
+    }
+
+    public float alcuadrado(int x){
+        return 0;
     }
 }
