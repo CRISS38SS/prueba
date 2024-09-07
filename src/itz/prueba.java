41e6a6c8b82fd64f2ac4e1a4d5df5ac9906119f2
Cristian Lopez <cl018191@gmail.com>
2024-09-07 12:07:20 -0600

    se agrego un metodo resta

--- src/itz/prueba.java
+++ src/itz/prueba.java
@@ -9,9 +9,14 @@ public class prueba {
         int x=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa un numero"));
         int y=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa un numero"));
         int sum=suma(x, y);
-        JOptionPane.showMessageDialog(null,sum);
+        int res=resta(x, y);
+        JOptionPane.showMessageDialog(null,"la suma es "+sum+" y la resta es "+res);
     }
     public int suma(int x,int y){
         return x+y;
+    }
+
+    public int resta(int x, int y){
+        return x-y;
     }
 }
