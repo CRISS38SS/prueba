9f2e81a8f991e1eb3453527ede25513f1c33dcdd
Cristian Lopez <cl018191@gmail.com>
2024-09-07 23:10:18 -0600

    se hizo mas legible

--- src/itz/prueba.java
+++ src/itz/prueba.java
@@ -1,22 +1,33 @@
 package itz;
 
 import javax.swing.JOptionPane;
+
 public class prueba {
+
     public static void main(String[] args) {
-        prueba n=new prueba();
-    }
-    public prueba(){
-        int x=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa un numero"));
-        int y=Integer.parseInt(JOptionPane.showInputDialog(null,"ingresa un numero"));
-        int sum=suma(x, y);
-        int res=resta(x, y);
-        JOptionPane.showMessageDialog(null,"la suma es "+sum+" y la resta es "+res);
-    }
-    public int suma(int x,int y){
-        return x+y;
+        prueba n = new prueba();
     }
 
-    public int resta(int x, int y){
-        return x-y;
+    public prueba() {
+        int x = Integer.parseInt(
+            JOptionPane.showInputDialog(null, "ingresa un numero")
+        );
+        int y = Integer.parseInt(
+            JOptionPane.showInputDialog(null, "ingresa un numero")
+        );
+        int sum = suma(x, y);
+        int res = resta(x, y);
+        JOptionPane.showMessageDialog(
+            null,
+            "la suma es " + sum + " y la resta es " + res
+        );
+    }
+
+    public int suma(int x, int y) {
+        return x + y;
+    }
+
+    public int resta(int x, int y) {
+        return x - y;
     }
 }
